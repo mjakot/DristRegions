ce56b0727e3d5cab06899a46a6e46314e2a05ccf
Mjakot <hlebletjaslomaju@gmail.com>
2024-09-30 00:21:32 +0300

    Updated OnCompassLinked

--- src/main/java/dristmine/dristregions/creation/OnCompassLinked.java
+++ src/main/java/dristmine/dristregions/creation/OnCompassLinked.java
@@ -1,9 +1,16 @@
 package dristmine.dristregions.creation;
 
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.domains.DefaultDomain;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
 import dristmine.dristregions.DristRegions;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
@@ -28,7 +35,7 @@ public class OnCompassLinked implements Listener {
 
 	@SuppressWarnings("ConstantConditions")
 	@EventHandler
-	public void onPlayerInteraction(PlayerInteractEvent event) {
+	public void onPlayerInteraction(PlayerInteractEvent event) throws ProtectedRegion.CircularInheritanceException {
 		if (!event.hasItem())
 			return;
 
@@ -36,9 +43,9 @@ public class OnCompassLinked implements Listener {
 			return;
 
 		if (
-				event.getAction() != Action.RIGHT_CLICK_BLOCK ||
-				event.getItem().getType() != Material.COMPASS ||
-				event.getClickedBlock().getType() != Material.LODESTONE
+			event.getAction() != Action.RIGHT_CLICK_BLOCK ||
+			event.getItem().getType() != Material.COMPASS ||
+			event.getClickedBlock().getType() != Material.LODESTONE
 		) {
 			return;
 		}
@@ -55,13 +62,31 @@ public class OnCompassLinked implements Listener {
 
 		Location regionOrigin = event.getClickedBlock().getLocation();
 
-		compassContainer.set(key, new UUIDDataType(), new UUID(0L, 0L));   //TODO: Add uuid system
+		UUID id = new UUID(0L, 0L);
+		compassContainer.set(key, new UUIDDataType(), id);   //TODO: Add uuid system
 
 		compassMeta.displayName(Component.text("Region access key"));             //TODO: config property
 		compassMeta.lore(List.of(new TextComponent[] { Component.text("TEST"), Component.text("TEST222") })); //TODO: config property
 
 		compassMeta.setLodestoneTracked(false);
 		compassMeta.setLodestone(regionOrigin);
+
+
+		int point1X = regionOrigin.getBlockX() - 5;
+		int point1Y = regionOrigin.getBlockY() - 5;
+		int point1Z = regionOrigin.getBlockZ() - 5;
+		BlockVector3 point1 = BlockVector3.at(point1X, point1Y, point1Z);
+		int point2X = regionOrigin.getBlockX() + 5;
+		int point2Y = regionOrigin.getBlockY() + 5;
+		int point2Z = regionOrigin.getBlockZ() + 5;
+		BlockVector3 point2 = BlockVector3.at(point2X, point2Y, point2Z);
+		ProtectedCuboidRegion region = new ProtectedCuboidRegion(id.toString(), point1, point2);
+		DefaultDomain owner = region.getOwners();
+		owner.addPlayer(event.getPlayer().getUniqueId());
+		region.setParent(null);
+		RegionContainer regionContainer = WorldGuard.getInstance().getPlatform().getRegionContainer();
+		RegionManager regionManager = regionContainer.get(BukkitAdapter.adapt(regionOrigin.getWorld()));
+		regionManager.addRegion(region);
 
 		compassInHand.setItemMeta(compassMeta);
 	}
